3da9875987488a5a4bfa9872315df80d68926056
Rafael del Molino <33693330+RafaeldelM@users.noreply.github.com>
2024-12-03 23:11:08 +0100

    Starting Project

---
+++ WpfApp1-Rhino8Core7/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Rhino.Geometry;
+using Rhino.Runtime.InProcess;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp1_Rhino8Core7
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            // Inicializa Rhino Inside
+            RhinoInside.Resolver.Initialize();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+            System.IO.Directory.SetCurrentDirectory(RhinoInside.Resolver.RhinoSystemDirectory);
+
+            using (new RhinoCore())
+            {
+                var sphere = new Sphere(Point3d.Origin, 12);
+                var brep = sphere.ToBrep();
+                var mp = new MeshingParameters(0.5);
+                var mesh = Mesh.CreateFromBrep(brep, mp);
+                /*Console.WriteLine($"Mesh with {mesh[0].Vertices.Count} vertices created");
+                Console.WriteLine("press any key to exit");
+                Console.ReadKey();*/
+            }
+         
+
+
+
+        }
+    }
+
+}
